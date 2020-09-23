e95dbc1f5b3b7db4d4c1d68afe15ef1ff0480167
Gabrielhlc <gabriel.carvalho@ccc.ufcg.edu.br>
2020-09-22 23:08:00 -0300

    A2U2

---
+++ A2U2_UnidadesEMedidas/unidades.py
@@ -0,0 +1,9 @@
+metro = float(input())
+#jarda = 0.9144 m
+#pes = 0.3048 m
+#polegada = 0.254 m 
+jarda = 1.09361 * metro
+pes = 3.28084 * metro 
+polegadas = 39.37008 * metro
+
+print(f'Jardas: {jarda:.3f} yd\nPés: {pes:.3f} ft\nPolegadas: {polegadas:.3f} in')
